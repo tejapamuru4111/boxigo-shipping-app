6b323f2a47c70237a6d3edc9b991bf60e8aedd8b
tejapamuru4111 <pamurutejayadav@gmail.com>
2023-04-04 13:26:01 +0530

    version 1.3

--- src/components/InventoryItem/index.js
+++ src/components/InventoryItem/index.js
@@ -9,37 +9,55 @@ const InventoryItem = (props) => {
 
     const [isExpand, setIsExpand] = useState(false)
 
-    console.log(inventory.category[0].items)
+    //console.log(inventory.category)
+
+    const qtyArr = inventory.category.map(item => item.items.map(obj => obj.qty))
+    let qty = 0
+    for (let arr of qtyArr){
+        qty += arr.reduce((a,v) => a + v)
+    }
+    //console.log(qty)
+
 
     const onExpandSection = () => {
         setIsExpand(!isExpand)
     }
 
     const expandDetails = () => (
-        <div className='inventory-items-card'>
-            {inventory.category.map(item => (
-                <ul key={item.id} className='inventory-items-card-list'>
-                    <h1 className='inventory-items-card-list-title'>{item.displayName}</h1>
-                    {item.items.map(itemName => {
-                        if (itemName.qty !== 0){
-                            return (
-                                <li key={itemName.id} className='inventory-items-card-list-item'>
-                                    <p className='inventory-items-card-list-item-name'>{itemName.displayName}<br></br></p>
-                                    <p className='inventory-items-card-list-item-count'>{itemName.qty}</p>
-                                </li>
-                            )
+            <div className='inventory-items-card'>
+                {qty === 0? <p className='inventory-items-card-list-title'>No Items</p>
+                     : 
+                     inventory.category.map(item => {
+                        const categoryItemQtyArr = item.items.map(arr => arr.qty)
+                        const categoryItemQtyArrQty = categoryItemQtyArr.reduce((a,v) => a+v)
+                        if (categoryItemQtyArrQty === 0){
+                            return ''
                         }
-                        return ''
-                    })}
-                </ul>
-            ))}
-        </div>
-    )
+                        return (
+                                <ul key={item.id} className='inventory-items-card-list'>
+                                    <h1 className='inventory-items-card-list-title'>{item.displayName}</h1>
+                                    {item.items.map(itemName => {
+                                        if (itemName.qty !== 0){
+                                            return (
+                                                <li key={itemName.id} className='inventory-items-card-list-item'>
+                                                    <p className='inventory-items-card-list-item-name'>{itemName.displayName} : </p>
+                                                    <p className='inventory-items-card-list-item-count'>{itemName.qty}</p>
+                                                </li>
+                                            )
+                                        }
+                                        return ''
+                                    })}
+                                </ul>
+                            )}
+                        )
+                }
+            </div>
+        )
 
     return (
         <li>
             <div className='house-furniture-ites-list-item'>
-                <h1 className='house-furniture-inventory-name'>{inventory.displayName}<span className='total-numbeer-of-items'>{inventory.category[0].items.length}</span></h1>
+                <h1 className='house-furniture-inventory-name'>{inventory.displayName}<span className='total-numbeer-of-items'>{qty}</span></h1>
                 <button type='button' className='house-furniture-inventory-btn' onClick={onExpandSection}>{isExpand? <AiOutlineUp /> : <AiOutlineDown />}</button>
             </div>
             {isExpand? expandDetails():''}
